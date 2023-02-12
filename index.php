cd44eb2e940d67db91cf7b82ea415c4d4d51f7e7
JalzAe <jalzae@gmail.com>
2023-02-12 10:54:22 +0700

    update uuid convert

--- index.php
+++ index.php
@@ -1,26 +1,53 @@
 <?php
 
-class Generate {
-  public static function uuid($str=""){
-    $str=(string)$str;
+class Generate
+{
+  public static function uuid($str = "")
+  {
+    $type = gettype($str);
+    if ($type != 'string') {
+      $str = (string)$str;
+    }
     try {
       $uuid = sprintf(
-    '%08x-%04x-%04x-%04x-%12x',
-    (hexdec(substr(md5($str), 0, 8)) & 0xffffffff),
-    (hexdec(substr(md5($str), 8, 4)) & 0xffff),
-    (hexdec(substr(md5($str), 12, 4)) & 0xffff),
-    (hexdec(substr(md5($str), 16, 4)) & 0x0fff) | 0x4000,
-    (hexdec(substr(md5($str), 20, 12)) & 0x3fffffffffff)
-);
-return $uuid;
+        '%08x-%04x-%04x-%04x-%12x',
+        (hexdec(substr(md5($str), 0, 8)) & 0xffffffff),
+        (hexdec(substr(md5($str), 8, 4)) & 0xffff),
+        (hexdec(substr(md5($str), 12, 4)) & 0xffff),
+        (hexdec(substr(md5($str), 16, 4)) & 0x0fff) | 0x4000,
+        (hexdec(substr(md5($str), 20, 12)) & 0x3fffffffffff)
+      );
+
+
+      return $uuid;
     } catch (Exception $e) {
       throw new Exception($e);
     }
   }
+
+  public static function Insert(array $data, string $key, string $target = "id")
+  {
+    $data[$target] = self::uuid($data[$key]);
+
+    return $data;
+  }
 }
 
-$uuid=Generate::uuid(1);
+echo "Sample \n";
 
-echo $uuid;
+$uuid = Generate::uuid(1);
 
-?>
+echo $uuid . "\n";
+
+$data = [
+  'id' => 1,
+  "name" => 'akmal'
+];
+echo "\n";
+print_r($data);
+
+echo "generate \n";
+
+$data = Generate::Insert($data, 'id');
+
+print_r($data);
